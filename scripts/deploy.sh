7fcf6462398de995091014f3bd158b6ca408fe18
Avrosh K <avroshkumar@gmail.com>
2016-07-30 17:14:52 -0400

    Shell Script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
 # Configuration
-BASE=/data/myapp
-TARGET=/someplace
+# BASE=/data/myapp
+# TARGET=/someplace
+
+#Compress app
+zip -r -X dist/app.zip app
+
+#Compress server
+zip -r -X dist/curriculum.zip curriculum
+
+#Compress curriculum
+zip -r -X dist/serverstuff.zip serverstuff
